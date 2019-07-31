688e3a0e3ba0df62c5c8a14a74604dd048e0b5ad
marviee <37642775+marviee@users.noreply.github.com>
2019-07-31 07:58:21 +0100

    Update Cipher.java

--- terragon_intern/Cipher.java
+++ terragon_intern/Cipher.java
@@ -7,10 +7,12 @@ public class Cipher{
 	public static void main(String[] args) {
         
 		//check the CipherCreation.java class for the methods to encrypt and decrypt
+		//the number of character shift is set to 4 but we can collect all this inputs(texts and shift num) from user using Scanner
+		
 		CipherCreation cipherCreation = new CipherCreation(4);
 		System.out.println(cipherCreation.encrypt("text to cipherr", 4));
         
-		//To decrypt
+		//To decrypt, uncomment the next line
 		//System.out.println(cipherCreation.decrypt("xibxdxsdgmtlivv", 4));
 	}
 }
